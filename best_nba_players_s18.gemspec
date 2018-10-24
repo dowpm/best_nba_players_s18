a24d7422e58f755ec8720a8ff8dda5d8bd298d6f
dowpm <mcdowpm@gmail.com>
2018-10-24 13:12:13 -0400

    require scraper class, open-uri, pry and nokogiri on best_nba_players_s18.rb

--- best_nba_players_s18.gemspec
+++ best_nba_players_s18.gemspec
@@ -35,4 +35,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.16"
   spec.add_development_dependency "rake", "~> 10.0"
 
+  spec.add_development_dependency "pry", "~> 0.10"
+  spec.add_development_dependency "nokogiri"
 end
--- lib/best_nba_players_s18.rb
+++ lib/best_nba_players_s18.rb
@@ -1,7 +1,12 @@
 require "best_nba_players_s18/version"
 
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
 module BestNbaPlayersS18
   # Your code goes here...
   require 'best_nba_players_s18/cli'
   require 'best_nba_players_s18/players'
+  require 'best_nba_players_s18/scraper'
 end
