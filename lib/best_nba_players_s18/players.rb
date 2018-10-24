57d5313e8c5a766a3044d267fe1280b4d10f9d16
dowpm <mcdowpm@gmail.com>
2018-10-24 16:28:09 -0400

    modified players class [initialize, create_players.. all]

--- lib/best_nba_players_s18/players.rb
+++ lib/best_nba_players_s18/players.rb
@@ -1,3 +1,26 @@
 class BestNbaPlayersS18::Players
+    attr_accessor :trend, :name, :team, :position, :statistics, :rank, :info
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def initialize infos_hash
+        infos_hash.each do |info, value|
+            self.send("#{info}=",value)
+        end
+        @@all << self
+    end
+
+    def self.create_players_from_collection collection
+        collection.each do |players|
+            players.each {|infos_hash| new(infos_hash) }
+        end
+    end
+
+    def self.find input
+        all[input-1]
+    end
 
 end
