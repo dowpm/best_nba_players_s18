58572dc8b8111df9c25a24ad406866c9d63bfe4a
dowpm <mcdowpm@gmail.com>
2018-10-24 13:09:18 -0400

    add code for scraping name, rank, position, and team for a player

--- lib/best_nba_players_s18/scraper.rb
+++ lib/best_nba_players_s18/scraper.rb
@@ -1,4 +1,14 @@
 class BestNbaPlayersS18::Scraper
 
+    def self.scrape_page url
+      nokogiri_url = Nokogiri::HTML(open(url))
+      players_neutral_nokogiri = nokogiri_url.search(".player-container.trend-neutral div.player-info-inner")
   
-end
+      player_name = players_neutral_nokogiri.css(".player-info-name").children[2].text.strip
+      player_rank = players_neutral_nokogiri.css(".player-info-name").children[1].text.strip
+      player_position, player_team = players_neutral_nokogiri.css(".player-info-team-and-positon").text.split(",")
+  
+      binding.pry
+    end
+  
+  end
